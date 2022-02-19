f6bd36992445d1a5a47d3b0232ae2d278bb194b4
Tamal Saha <tamal@appscode.com>
2022-02-18 18:13:24 -0800

    Update import-crds script
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/import-crds.sh
+++ hack/scripts/import-crds.sh
@@ -2,12 +2,12 @@
 set -eou pipefail
 
 crd-importer \
---input=$HOME/go/src/kmodules.xyz/custom-resources/crds \
+--input=https://raw.githubusercontent.com/kmodules/custom-resources/3bf3dbd8ac52976bebfe47e91c675ecd4da3b3a3/crds/appcatalog.appscode.com_appbindings.yaml \
 --out=$HOME/go/src/github.com/ak8sdb/installer/charts/kubedb-crds/crds \
 --gk=AppBinding.appcatalog.appscode.com
 
 crd-importer \
---input=$HOME/go/src/kubedb.dev/apimachinery/crds \
+--input=$HOME/go/src/github.com/ak8sdb/kubedb-apimachinery/crds \
 --out=$HOME/go/src/github.com/ak8sdb/installer/charts/kubedb-crds/crds \
 --gk=Elasticsearch.kubedb.com \
 --gk=ElasticsearchAutoscaler.autoscaling.kubedb.com \
@@ -19,7 +19,7 @@ crd-importer \
 --gk=MongoDBVersion.catalog.kubedb.com
 
 crd-importer \
---input=$HOME/go/src/stash.appscode.dev/apimachinery/crds \
+--input=$HOME/go/src/github.com/ak8sdb/stash-apimachinery/crds \
 --out=$HOME/go/src/github.com/ak8sdb/installer/charts/kubedb-crds/crds \
 --gk=BackupConfiguration.stash.appscode.com \
 --gk=BackupSession.stash.appscode.com \
@@ -28,12 +28,11 @@ crd-importer \
 --gk=RestoreSession.stash.appscode.com \
 --gk=Task.stash.appscode.com
 
-
 crd-importer \
---input=$HOME/go/src/kmodules.xyz/custom-resources/crds \
+--input=https://raw.githubusercontent.com/kmodules/custom-resources/3bf3dbd8ac52976bebfe47e91c675ecd4da3b3a3/crds/appcatalog.appscode.com_appbindings.yaml \
 --out=. --output-yaml=kubedb-crds.yaml \
 --gk=AppBinding.appcatalog.appscode.com \
---input=$HOME/go/src/kubedb.dev/apimachinery/crds \
+--input=$HOME/go/src/github.com/ak8sdb/kubedb-apimachinery/crds \
 --gk=Elasticsearch.kubedb.com \
 --gk=ElasticsearchAutoscaler.autoscaling.kubedb.com \
 --gk=ElasticsearchOpsRequest.ops.kubedb.com \
@@ -42,7 +41,7 @@ crd-importer \
 --gk=MongoDBAutoscaler.autoscaling.kubedb.com \
 --gk=MongoDBOpsRequest.ops.kubedb.com \
 --gk=MongoDBVersion.catalog.kubedb.com \
---input=$HOME/go/src/stash.appscode.dev/apimachinery/crds \
+--input=$HOME/go/src/github.com/ak8sdb/stash-apimachinery/crds \
 --gk=BackupConfiguration.stash.appscode.com \
 --gk=BackupSession.stash.appscode.com \
 --gk=Function.stash.appscode.com \
