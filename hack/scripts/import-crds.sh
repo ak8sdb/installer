de61cfe7a89f5e158ed4dafbb8dfa78472c68861
Tamal Saha <tamal@appscode.com>
2022-03-30 04:34:20 -0700

    Update crds
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/import-crds.sh
+++ hack/scripts/import-crds.sh
@@ -7,7 +7,7 @@ crd-importer \
     --gk=AppBinding.appcatalog.appscode.com
 
 crd-importer \
-    --input=$HOME/go/src/github.com/ak8sdb/kubedb-apimachinery/crds \
+    --input=$HOME/go/src/github.com/ak8sdb/kubedb/apimachinery/crds \
     --out=$HOME/go/src/github.com/ak8sdb/installer/charts/kubedb-crds/crds \
     --gk=Elasticsearch.kubedb.com \
     --gk=ElasticsearchAutoscaler.autoscaling.kubedb.com \
@@ -19,7 +19,7 @@ crd-importer \
     --gk=MongoDBVersion.catalog.kubedb.com
 
 crd-importer \
-    --input=$HOME/go/src/github.com/ak8sdb/stash-apimachinery/crds \
+    --input=$HOME/go/src/github.com/ak8sdb/stash/apimachinery/crds \
     --out=$HOME/go/src/github.com/ak8sdb/installer/charts/kubedb-crds/crds \
     --gk=BackupConfiguration.stash.appscode.com \
     --gk=BackupSession.stash.appscode.com \
@@ -32,7 +32,7 @@ crd-importer \
     --input=https://raw.githubusercontent.com/kmodules/custom-resources/3bf3dbd8ac52976bebfe47e91c675ecd4da3b3a3/crds/appcatalog.appscode.com_appbindings.yaml \
     --out=. --output-yaml=kubedb-crds.yaml \
     --gk=AppBinding.appcatalog.appscode.com \
-    --input=$HOME/go/src/github.com/ak8sdb/kubedb-apimachinery/crds \
+    --input=$HOME/go/src/github.com/ak8sdb/kubedb/apimachinery/crds \
     --gk=Elasticsearch.kubedb.com \
     --gk=ElasticsearchAutoscaler.autoscaling.kubedb.com \
     --gk=ElasticsearchOpsRequest.ops.kubedb.com \
@@ -41,7 +41,7 @@ crd-importer \
     --gk=MongoDBAutoscaler.autoscaling.kubedb.com \
     --gk=MongoDBOpsRequest.ops.kubedb.com \
     --gk=MongoDBVersion.catalog.kubedb.com \
-    --input=$HOME/go/src/github.com/ak8sdb/stash-apimachinery/crds \
+    --input=$HOME/go/src/github.com/ak8sdb/stash/apimachinery/crds \
     --gk=BackupConfiguration.stash.appscode.com \
     --gk=BackupSession.stash.appscode.com \
     --gk=Function.stash.appscode.com \
